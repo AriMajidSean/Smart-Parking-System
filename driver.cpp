77be0d69c654e09a025abf8004e67a84318b74ae
Hussein-Watfa <husseinwatfa101@gmail.com>
2025-11-11 13:53:40 -0500

    sprint 1- getting started on sensor usage and distance calculation (Hussein)

--- driver.cpp
+++ driver.cpp
@@ -1,3 +1,39 @@
 /**
 * driver.cpp
 */
+
+#include <Arduino.h>
+
+// parking spot struct, carries ID of spot, whether it is occupied, and sensor information to determine if a car is there
+struct ParkingSpot {
+    int spotID;
+    int trigPin;
+    int echoPin;
+    int defaultEmptyDistance;
+    bool isOccupied;
+};
+
+// tells the microcontroller which pin sends or recieves information
+void setup() {
+    pinMode(trigPin, OUTPUT);
+    pinMode(echoPin, INPUT);
+}
+
+// function that returns the distance an object is from the ultrasonic sensor
+float measuredDistance(int trigPin, int echoPin) {
+    // clears the trigPin
+    digitalWrite(trigPin, LOW);
+    delayMicroseconds(2);
+
+    // sends a pulse
+    digitalWrite(trigPin, HIGH);
+    delayMircroseconds(10);
+    digitalWrite(trigPin, LOW);
+
+    // measures the time it took for the singal to return
+    int duration = pulseIn(echoPin, HIGH);
+
+    // calculates the distance in cm: (duration * speed of sound) / 2
+    float distance = duration * 0.0343 / 2;
+    return distance;
+}
